282e5ce04aef4ed3d8fc12425e136925b3ca4618
Jensen Huang <jensen@huangyankai.com>
2021-03-01 17:34:02 +0800

    dont understand whats going on why does the thing not work???

--- ds1820.ts
+++ ds1820.ts
@@ -1,5 +1,7 @@
 //% color=#27b0ba icon="\uf26c" block="Basic Blocks"
 namespace ds1820 {
+    
+    
     function resetDS1820(DSpin: DigitalPin){
         pins.digitalWritePin(DSpin,0)
         control.waitMicros(500)
@@ -8,13 +10,12 @@ namespace ds1820 {
     }
 
     function writeDS1820(DSpin: DigitalPin, data: Number){
-        let dataString = data.toString()
-        let bytes = parseInt(dataString, 16);
-        for (let i = 0; i < dataString.length; i++){
-            let bit = dataString[i]
+        let binValueString = data.toString()
+        for (let i = 0; i < binValueString.length; i++){
+            let bit = binValueString[i]
             if (parseInt(bit) == 1){
                 pins.digitalWritePin(DSpin,0)
-                control.waitMicros(10)
+                control.waitMicros(6)
                 pins.digitalReadPin(DSpin)
                 control.waitMicros(60)
             }
@@ -40,21 +41,31 @@ namespace ds1820 {
         
         return reply
     }
-
+    
+    //dumb workaround since microbit somehow doesnt support toString(16)
+    let ZeroXFortyFour = 1000100
+    let ZeroXCC = 11001100
+    let ZeroXBE = 10111110
     //% block="hello get pin on %DSPin"
     //% weight=3
     export function tempDS1820(DSPin: DigitalPin){
         resetDS1820(DSPin)
-        writeDS1820(DSPin,0xCC)
-        writeDS1820(DSPin,0x44)
+        writeDS1820(DSPin, ZeroXCC)
+        writeDS1820(DSPin, ZeroXFortyFour)
+        resetDS1820(DSPin)
+        writeDS1820(DSPin, ZeroXCC)
+        writeDS1820(DSPin, ZeroXBE)
         //reading temperature
+    
         let temp = 0
-        for (let i = 0; i<2; i++){
-            let response = readDS1820(DSPin)
-            temp = (temp << 8)| response
-        }
-        return temp
+        let byte0 = readDS1820(DSPin)
+        let byte1 = readDS1820(DSPin)
+        resetDS1820(DSPin)
+        temp = (byte1 << 8)| byte0
+
+        temp = temp >> 4
+        let tempString = temp.toString()
+        return tempString
 
     }
 }
-
