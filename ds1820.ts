08269a572b10faa6ca56a9e9778512539fb9daaf
Jensen Huang <jensen@huangyankai.com>
2021-03-02 17:50:45 +0800

    improved stuff maybe?

--- ds1820.ts
+++ ds1820.ts
@@ -1,12 +1,28 @@
 //% color=#27b0ba icon="\uf26c" block="Basic Blocks"
 namespace ds1820 {
-    
+    function makeHigh(DSpin: DigitalPin){
+        pins.digitalReadPin(DSpin)
+    }
+
     
     function resetDS1820(DSpin: DigitalPin){
+        control.waitMicros(10)
+        let detected = 1
+        pins.setPull(DSpin,PinPullMode.PullUp)
         pins.digitalWritePin(DSpin,0)
-        control.waitMicros(500)
-        let response:Number = pins.digitalReadPin(DSpin)
-        return response
+        control.waitMicros(480)
+        pins.digitalReadPin(DSpin)
+        control.waitMicros(70)
+        detected = pins.digitalReadPin(DSpin)
+        control.waitMicros(480)
+        if (detected == 1){
+            serial.writeLine("trash device gg uninstall")
+            //TLDR detected variable should equals zero when device is actually detected
+        }
+        else{
+            serial.writeLine("Connected!")
+        }
+        return detected
     }
 
     function writeDS1820(DSpin: DigitalPin, data: Number){
@@ -17,26 +33,33 @@ namespace ds1820 {
                 pins.digitalWritePin(DSpin,0)
                 control.waitMicros(6)
                 pins.digitalReadPin(DSpin)
-                control.waitMicros(60)
+                control.waitMicros(65)
             }
             if (parseInt(bit) == 0){
                 pins.digitalWritePin(DSpin,0)
-                control.waitMicros(60)    
-                control.waitMicros(2)
+                control.waitMicros(60)
+                pins.digitalReadPin(DSpin)    
+                control.waitMicros(10)
             }
         }
     }
     
-    function readDS1820(DSpin: DigitalPin){
-        pins.digitalWritePin(DSpin,0)
+    function readBitDS1820(DSpin: DigitalPin){
+        let data:number
         control.waitMicros(1)
+        pins.setPull(DSpin,PinPullMode.PullUp)
+        control.waitMicros(12)
+        data = pins.digitalReadPin(DSpin)
+        control.waitMicros(47)
+        return data
+    }
+    function readByteDS1820(DSpin: DigitalPin){
+        //reading 8 bits then combines into a byte
+        
         let reply = 0
         for (let i = 0;i<8;i++){
-            //read 8 bits
-            control.waitMicros(1)
-            let ack = pins.digitalReadPin(DSpin)
-            reply = (reply << 1) | ack     
-            control.waitMicros(60)
+            let bit = readBitDS1820(DSpin)
+            reply =  reply << 1 | bit
         }
         
         return reply
@@ -52,20 +75,22 @@ namespace ds1820 {
         resetDS1820(DSPin)
         writeDS1820(DSPin, ZeroXCC)
         writeDS1820(DSPin, ZeroXFortyFour)
+        control.waitMicros(750)
         resetDS1820(DSPin)
         writeDS1820(DSPin, ZeroXCC)
         writeDS1820(DSPin, ZeroXBE)
         //reading temperature
     
         let temp = 0
-        let byte0 = readDS1820(DSPin)
-        let byte1 = readDS1820(DSPin)
+        let byte0 = readByteDS1820(DSPin)
+        let byte1 = readByteDS1820(DSPin)
         resetDS1820(DSPin)
         temp = (byte1 << 8)| byte0
 
-        temp = temp >> 4
+        //temp = temp >> 4
         let tempString = temp.toString()
         return tempString
+        serial.writeLine(temp.toString())
 
     }
 }
